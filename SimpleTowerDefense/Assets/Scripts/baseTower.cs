ac53b6840cb149a6c16b424a22ca34b4d1fdcc40
MarvinHawkins <m.louis.hawkins@gmail.com>
2016-01-25 22:14:50 -0600

    Junk Check in
    May delete

---
+++ SimpleTowerDefense/Assets/Scripts/baseTower.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+
+
+public class baseTower : MonoBehaviour {
+
+
+
+
+
+
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ SimpleTowerDefense/Assets/Scripts/towerData.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class towerLevel
+{
+    
+
+}
+
+public class towerData : MonoBehaviour {
+
+    public int cost;
+    public GameObject appearance; //Determines how t he unit looks
+    public float range; //how far can the unit see?
+    public List<towerLevel> levels;
+    private towerLevel currentLevel;
+
+    void OnEnable()
+    {
+        currentLevel = levels[0];
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
